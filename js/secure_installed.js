16547f966b7b4341acb0302b7fd3f86f98a9e5d7
Joseph Marsden <josephmarsden@towerdevs.xyz>
2016-09-30 12:32:02 +0930

    Fixed a few things

--- js/secure_installed.js
+++ js/secure_installed.js
@@ -1,2 +1,4 @@
+function redirectSecure() {
 window.location.replace('http://untangle.tower.local/capture/handler.py/index?appid=14');
 window.location.href = "http://untangle.tower.local/capture/handler.py/index?appid=14";
+}
