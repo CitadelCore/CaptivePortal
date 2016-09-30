88ca8a783671ae897706143fb8f6aba64e1921cb
Joseph Marsden <josephmarsden@towerdevs.xyz>
2016-09-30 12:19:18 +0930

    Updated certificate installation page

---
+++ js/secure_installed.js
@@ -0,0 +1,2 @@
+window.location.replace('http://untangle.tower.local/capture/custom_14/certificate_install.html');
+window.location.href = "http://untangle.tower.local/capture/custom_14/certificate_install.html";
